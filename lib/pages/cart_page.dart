64029985040f81540159054875710e3a62936f87
raju-islam <rajuslam39@gmail.com>
2023-12-24 10:36:26 +0600

    product add

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -3,9 +3,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class CartPage extends StatelessWidget {
+import '../models/shop.dart';
+
+class CartPage extends StatefulWidget {
   const CartPage({super.key});
 
+  @override
+  State<CartPage> createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage> {
+  Shop _shop=Shop();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,6 +22,52 @@ class CartPage extends StatelessWidget {
         title: Text("Cart",style: TextStyle(color: Colors.white),),
         centerTitle: true,
       ),
+      body: ListView.builder(
+        itemCount: _shop.cartList.length,
+          itemBuilder: (context,index){
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10)),
+              height: 200,
+              width: 150,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Image.network(
+                    _shop.cartList[index]["image"],
+                    height: 120,
+                  ),
+                  // SizedBox(
+                  //   height: 20,
+                  // ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text(_shop.cartList[index]["productName"]),
+                      InkWell(
+                        onTap: () {},
+                        child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.indigo,
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: Icon(
+                                Icons.add,
+                                color: Colors.white,
+                              ),
+                            )),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          );
+          }),
     );
   }
 }
